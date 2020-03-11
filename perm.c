fa4176913cc9c5c91e2ecaaac8d12fb7c212325b
Luis Colorado <luiscoloradourcola@gmail.com>
2020-03-11 17:02:44 +0200

    intern all equal strings into same pointer
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- perm.c
+++ perm.c
@@ -5,19 +5,18 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <getopt.h>
 
 
-bool ex(char **a, char **b)
+void ex(char **a, char **b)
 {
-	if (a == b) return false;
+	if (a == b) return;
 
 	char *p = *a;
 	*a = *b;
 	*b = p;
-
-	return true;
 }
 
 char **all;
@@ -42,9 +41,9 @@ void permuta(int argc, char **argv)
 		return;
 	}
 	for (i = 0; i < argc; i++) {
-		bool did_ex = ex(&argv[0], &argv[i]);
+		ex(&argv[0], &argv[i]);
 		permuta(argc-1, argv+1);
-		if (did_ex) ex(&argv[0], &argv[i]);
+		ex(&argv[0], &argv[i]);
 	}
 }
 
@@ -61,6 +60,18 @@ int main(int argc, char **argv)
 
 	argc -= optind; argv += optind;
 	all = argv;
+
+	/* the following code "interns" all strings, so
+	 * if the pointers are equal, the strings are equal,
+	 * without the need to allocate memory */
+	int i;
+	for(i = 0; i < argc-1; i++) {
+		int j;
+		for (j = i + 1; j < argc; j++) {
+			if (0 == strcmp(argv[i], argv[j]))
+				argv[j] = argv[i];
+		}
+	}
 
 	permuta(argc, argv);
 	puts("");
